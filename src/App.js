ffe6b7a1d2c08af314a7bb06b471f59bb1ad2e8b
Tim <timothysalacup@gmail.com>
2023-04-11 22:31:47 +0800

    finished library react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books.jsx";
+import { books } from "./data.js";
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => {
+        return item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item;
+      })
+    );
+  }
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log('removeItem', item)
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()}/>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} addToCart={addToCart} cart={cart}/>}
+          />
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Featured.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Book from './ui/Book.jsx';
+import { books } from "../data.js";
+
+const Featured = () => {
+  console.log(books);
+  console.log();
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="purple">Books</span>
+          </h2>
+          <div className="books">
+            {books
+              .filter((books) => books.rating === 5)
+              .slice(0, 4)
+              .map((book) => (
+                <Book book={book} key={book.id} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
